813d11630f9bad5856af29f01076ee8f74813259
Tomasz Siudut <tomek.siudut11@gmail.com>
2021-04-22 14:38:17 +0200

    custom image tile added

--- lib/screens/search_images_screen.dart
+++ lib/screens/search_images_screen.dart
@@ -1,10 +1,11 @@
 import "package:flutter/material.dart";
+import 'package:images/widgets/image_tile.dart';
 
 class SearchImagesScreen extends StatelessWidget {
   const SearchImagesScreen({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("Home Screen with Search")));
+    return Scaffold(body: Center(child: ImageTile()));
   }
 }
---
+++ lib/widgets/image_tile.dart
@@ -0,0 +1,50 @@
+import "package:flutter/material.dart";
+
+class ImageTile extends StatelessWidget {
+  const ImageTile({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+        borderRadius: BorderRadius.circular(8.0),
+        child: Container(
+          color: Colors.white,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Image.network(
+                "https://ireland.apollo.olxcdn.com/v1/files/goxzdbthwbkt-PL/image;s=644x461",
+                height: 250.0,
+                width: 200.0,
+                fit: BoxFit.fitWidth,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Text(
+                            "Image Title",
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ),
+                        SizedBox(width: 8),
+                      ],
+                    ),
+                    SizedBox(height: 2),
+                    Text(
+                      "image description",
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(color: Colors.grey, fontSize: 12),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ));
+  }
+}
