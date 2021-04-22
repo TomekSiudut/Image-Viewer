e3e3af672d72b06bb699c843e466d55785fe11eb
Tomasz Siudut <tomek.siudut11@gmail.com>
2021-04-22 18:13:34 +0200

    details screen completed

---
+++ lib/screens/details_screen.dart
@@ -0,0 +1,38 @@
+import "package:flutter/material.dart";
+import 'package:images/theme/colors.dart';
+
+class DetailsScreen extends StatelessWidget {
+  final String imageUrl;
+  const DetailsScreen({Key key, this.imageUrl}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: AppColors.mainColor,
+      ),
+      body: Stack(children: <Widget>[
+        Image.network(
+          imageUrl,
+          fit: BoxFit.cover,
+          height: double.infinity,
+          width: double.infinity,
+          alignment: Alignment.center,
+        ),
+        Padding(
+          padding: const EdgeInsets.all(35.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              Icon(
+                Icons.favorite_border_outlined,
+                color: Colors.white,
+                size: 35.0,
+              )
+            ],
+          ),
+        ),
+      ]),
+    );
+  }
+}
--- lib/widgets/image_tile.dart
+++ lib/widgets/image_tile.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import 'package:images/screens/details_screen.dart';
 
 class ImageTile extends StatelessWidget {
   final String imageUrl;
@@ -8,48 +9,57 @@ class ImageTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-        borderRadius: BorderRadius.circular(8.0),
-        child: Container(
-          color: Colors.white,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Expanded(
-                child: Image.network(
-                  imageUrl,
-                  height: 250.0,
-                  width: 200.0,
-                  fit: BoxFit.fill,
+    return GestureDetector(
+      onTap: () => Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => DetailsScreen(
+                  imageUrl: imageUrl,
+                )),
+      ),
+      child: ClipRRect(
+          borderRadius: BorderRadius.circular(8.0),
+          child: Container(
+            color: Colors.white,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: Image.network(
+                    imageUrl,
+                    height: 250.0,
+                    width: 200.0,
+                    fit: BoxFit.fill,
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      children: [
-                        Expanded(
-                          child: Text(
-                            imageTitle,
-                            overflow: TextOverflow.ellipsis,
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Expanded(
+                            child: Text(
+                              imageTitle,
+                              overflow: TextOverflow.ellipsis,
+                            ),
                           ),
-                        ),
-                        SizedBox(width: 8),
-                      ],
-                    ),
-                    SizedBox(height: 2),
-                    Text(
-                      "image description",
-                      overflow: TextOverflow.ellipsis,
-                      style: TextStyle(color: Colors.grey, fontSize: 12),
-                    ),
-                  ],
+                          SizedBox(width: 8),
+                        ],
+                      ),
+                      SizedBox(height: 2),
+                      Text(
+                        "image description",
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(color: Colors.grey, fontSize: 12),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            ],
-          ),
-        ));
+              ],
+            ),
+          )),
+    );
   }
 }
