4dbe30fcab328c19531da88e5fd0e682ac631028
Tomasz Siudut <tomek.siudut11@gmail.com>
2021-04-22 20:38:17 +0200

    empty list message added

--- lib/widgets/empty_list.dart
+++ lib/widgets/empty_list.dart
@@ -2,16 +2,18 @@ import "package:flutter/material.dart";
 
 class EmptyList extends StatelessWidget {
   final Widget child;
-
   const EmptyList({Key key, this.child}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Center(
         child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
-        Icon(Icons.close, color: Colors.grey, size: 35.0),
-        Text("No images found", style: TextStyle(color: Colors.black))
+        Icon(Icons.image, color: Colors.grey, size: 50.0),
+        SizedBox(height: 10.0),
+        Text("No images found :(",
+            style: TextStyle(color: Colors.grey, fontWeight: FontWeight.bold))
       ],
     ));
   }
